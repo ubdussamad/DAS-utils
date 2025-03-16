e074abb60a297d15066be2fee9c4e13908efc961
Samad (Sam) <samadhaque23@gmail.com>
2025-03-16 13:36:47 -0500

    Update DetectStopSign_StopCar.py

--- LAB_9_Object_Detection_and_Identification/DetectStopSign_StopCar.py
+++ LAB_9_Object_Detection_and_Identification/DetectStopSign_StopCar.py
@@ -1,31 +1,17 @@
-
-
-###############################
-#### Stop Car at Stop Sign ####
-#### --------------------- ####
-#### Objective:        ####
-#### > Drive car forward   ####
-####   until a sotpsign is ####
-####   spotted on the cam  ####
-###############################
 import sys
 import os
 import cv2
-import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-enA=13
-in1=27
-in2=17
-GPIO.setup(enA,GPIO.OUT)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-pwm_motor=GPIO.PWM(enA,1000)
-pwm_motor.start(0)
+# Detect Stop Sign Via RPi cam
+# Note: There's a delay between what you see on the terminal, so expect some lag
+# in stop sign detection
 
-cascade_path = '/home/pi/TrafficLightDetection/stopsign_good.xml'
+shell_color_green = "\033[0;32m"
+shell_color_red =   "\033[0;31m"
+shell_color_reset = "\033[0m"
+
+cascade_path = 'stopsign_good.xml'
 
 if not os.path.exists(cascade_path):
 	print(f"Error: The file {cascade_path} does not exist.")
@@ -36,82 +22,59 @@ stop_sign_cascade = cv2.CascadeClassifier(cascade_path)
 if stop_sign_cascade.empty():
 	print("Error loading stop_sign_good.xml")
 	sys.exit()
-else:
-	print("Stop sign cascade loaded successfully.")
+else:print("Model: Stop sign cascade loaded successfully.")
+
+is_car_running = True
 
 
-def stop_car():
-	print("Stopping the car!")
-	GPIO.output(in1, GPIO.LOW)
-	GPIO.output(in2, GPIO.LOW)
-	pwm_motor.ChangeDutyCycle(0)
-
-def move_car():
-	print("Moving the car!")
-	GPIO.output(in1, GPIO.HIGH)
-	GPIO.output(in2, GPIO.LOW)	
-	pwm_motor.ChangeDutyCycle(100)
 
 def main():
+	global is_car_running
+	
 	cap = cv2.VideoCapture(0)
 
 	if not cap.isOpened():
 		print("Error: Could not open camera.")
 		sys.exit()
+	
+	print("Vechile is:", end="" , flush=True)
+	status_buffer = ""
 
-	#stop_sign_detected = False
-
-# Begin Camera video and driving forward #
 	while True:
+		__len_to_clear = len(status_buffer) - len(shell_color_reset) - len(shell_color_green)
+		# Cleans out the last status string
+		print( '\b' * __len_to_clear , " " * (__len_to_clear-2) , '\b' * __len_to_clear , end="") # Clear status buffer
+		
 		ret, frame = cap.read()
 		if not ret:
 			print("Error: could not read frame.")
 			break
-
-    # Resize the ROI for faster processing
+		# Resize the ROI for faster processing
 		frame = cv2.resize(frame,(640, 480))
-
-    # Convert to grayscale
+		# Convert to grayscale
 		gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect stop signs
+		# Detect stop signs
 		stop_signs = stop_sign_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-	
-	#if len(stop_signs) > 0:
-		#stop_sign_detected = True
-		for (x, y, w, h) in stop_signs:
-			roi = frame[y:y+h, x:x+w]
-			stop_car()
-			cv2.rectangle(frame, (x , y), (x + w, y + h), (255,0, 0), 2)
-			cv2.putText(frame, "Stop Sign", (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,  (255,0,0), 2)
-			#stop_car()
-			#sleep(1)
-			#break
 
-	#else:
-		#if stop_sign_detected:
-			#print("stop sign removed, moving forward.")
-			#move_car()
-			#stop_sign_detected = False
+		# If the stop_signs list has something in it, it means that a stop sign was detected
+		if (len(stop_signs)):
+			status_buffer = shell_color_red + "Stopped at Stop sign!" + shell_color_reset
+			print( status_buffer , flush=True  , end="")
+			is_car_running = False
+			# For future refrence,
+			# You can add a call back or set a flag for the actual rover to stop;
+			# the rover will have a control loop, you can pause that
+			# loop with a flag
 
-
-		if os.environ.get("DISPLAY"," ") == " ":
-			print("Running in headless mode, skipping display.")
-		else:
-			cv2.imshow("Stop Sign Detection", frame)
-			sleep(1)
-			if cv2.waitkey(1) & 0xFF ==ord("q"):
-				break
+			sleep(1) # you should wait for few seconds before checking again
+		else: # No stop sign detected
+			status_buffer = shell_color_green + "Moving!" + shell_color_reset
+			print( status_buffer , flush=True , end="")
+			is_car_running = True
 
 	cap.release()
 	cv2.destroyAllWindows()
-	GPIO.cleanup()
+
 
 if __name__ == "__main__":
-	#try:
-	move_car()
 	main()
-	#except KeyboardInterrupt:
-	#	print("Existing programs...")
-	#	GPIO.cleanup()
-	#	sys.exit()
